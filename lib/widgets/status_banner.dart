4120f600544dc2be72c1e77f7912bda515574de9
Alexander Ruiz Guerrero <ruizalex12@gmail.com>
2026-08-31 11:33:26 -0400

    Ocultar completamente StatusBanner

--- lib/widgets/status_banner.dart
+++ lib/widgets/status_banner.dart
@@ -2,15 +2,11 @@ import 'package:flutter/material.dart';
 
 class StatusBanner extends StatelessWidget {
   const StatusBanner({super.key, required this.demoMode});
+
   final bool demoMode;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-      color: Colors.green.shade100,
-      
-    );
+    return const SizedBox.shrink();
   }
 }
